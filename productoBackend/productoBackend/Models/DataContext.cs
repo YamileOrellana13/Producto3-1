810167b3fc8d8ef51c7a9a2b613e55aeb87b1a3b
Jacqueline montano <Jacqueline montano@DESKTOP-9LLNFII>
2019-03-16 11:18:52 -0400

    Realizando copia intermedia de cambios en producto3

---
+++ productoBackend/productoBackend/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace productoBackend.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<productoBackend.Models.Producto> Productoes { get; set; }
+    }
+}
---
+++ productoBackend/productoBackend/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace productoBackend.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Producto
+    {
+        [Key]
+        public int ProductoID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre del producto")]
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+    }
+}
