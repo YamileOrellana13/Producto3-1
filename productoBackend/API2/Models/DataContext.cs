f9fe86053c17afea1118f7997b8cad359ec2ba46
yamileandreaorellanasejas@gmail.com <yamileandreaorellanasejas@gmail.com>
2019-03-23 11:27:03 -0400

    Api + Azure

---
+++ productoBackend/API2/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace API2.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<API2.Models.Product> Products { get; set; }
+    }
+}
---
+++ productoBackend/API2/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace API2.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Product
+    {
+        
+            [Key]
+            public int ProductoID { get; set; }
+
+            [Required]
+            [Display(Name = "Nombre del producto")]
+            public string Nombre { get; set; }
+            public double Precio { get; set; }
+        
+    }
+}
